528d13b861dd83c87f08a55b654c9f9be5365403
Shivananda Reddy Kankanala <cyberllama13@gmail.com>
2025-06-12 17:49:36 +0530

    working ls woth Nerd Font

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
 use std::path::PathBuf;
+use crossterm::{style::{Color, Print, ResetColor, SetForegroundColor}, execute};
+use std::io::stdout;
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -8,7 +11,36 @@ struct Args {
     path: PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    println!("You want to list the contents of: {:?}", args.path);
+
+    for entry in fs::read_dir(args.path)? {
+        let entry = entry?;
+        let path = entry.path();
+        let file_name = path.file_name().unwrap_or_default().to_string_lossy();
+
+        if path.is_dir() {
+            // Directory: Print in blue with a folder icon
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::Blue),
+                Print(" "), // Nerd Font icon for folder
+                Print(file_name),
+                ResetColor,
+                Print("\n")
+            )?;
+        } else {
+            // File: Print in white with a file icon
+            execute!(
+                stdout(),
+                SetForegroundColor(Color::White),
+                Print(" "), // Nerd Font icon for file
+                Print(file_name),
+                ResetColor,
+                Print("\n")
+            )?;
+        }
+    }
+
+    Ok(())
 }
